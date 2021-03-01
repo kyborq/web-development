b05ab47664ebc28e59398d8e5e0433febb5fb600
Konstantin <k.podyganov@mail.ru>
2021-03-01 17:11:04 +0300

    Refactored

--- lw3/task1.php
+++ lw3/task1.php
@@ -1,17 +1,21 @@
 <?
-  header("Content-Type: text/plain");
+	header("Content-Type: text/plain");
 
-  // $string = preg_replace("/\s+/", " ", trim($_GET["text"], " "));
-  // echo "<pre>".$string."</pre>";
+	// $string = preg_replace("/\s+/", " ", trim($_GET["text"], " "));
+	// echo "<pre>".$string."</pre>";
 
-  $string = trim($_GET["text"], " ");
-  $count = 0;
-  
-  for ($i = 0; $i < strlen($string); $i++) {
-    if ($string[$i] === " " && $count < 2) {
-      $count += 1;
-    } else {
-      $count = 0;
-      echo $string[$i];
-    }
-  }
+	$string = trim($_GET["text"], " ");
+	$count = 0;
+	  
+	for ($i = 0; $i < strlen($string); $i++) 
+	{
+		if ($string[$i] === " " && $count < 2) 
+		{
+			$count += 1;
+		} 
+		else 
+		{
+			$count = 0;
+			echo $string[$i];
+		}
+	}
